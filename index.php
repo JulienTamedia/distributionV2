8622cb354f67ec9c91a890d46789bf8de4fbf314
comatik <loic.mancino.private@gmail.com>
2015-06-03 14:19:55 +0200

    import from loic

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-asdasd
-hello world
-asdadas
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet/less" type="text/css" href="./assets/css/style.less">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Include jQuery Mobile stylesheets -->
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<!-- Include the jQuery library -->
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<!-- Include the jQuery Mobile library -->
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<script src="./assets/js/less.js" type="text/javascript"></script>
+	<title>
+		Distribution
+	</title
+</head>
+<body>
+	<div id="panel1" class="ui-grid-solo panel">
+		<div class="ui-block-a"><div class="ui-bar ui-bar-a" id ="epaper">
+			<div class="layer">
+			<center>
+					<h1 id="texte_epaper">
+					Les informations quotidiennes <br /> <b>en un clin d'oeil</b>
+				</h1>
+				<img src="./assets/img/doigts.png">
+				</center>
+			</div>
+		</div></div>
+	</div>
+</body>
+</html>
