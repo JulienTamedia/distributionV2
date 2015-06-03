41ab9215f1d7ed3698dfc082d79ee120665936ae
comatik <loic.mancino.private@gmail.com>
2015-06-03 17:37:21 +0200

    Fin de journée

---
+++ includes/epaper.php
@@ -0,0 +1,8 @@
+			<div class="layer">
+			<center>
+					<h1 id="texte_epaper">
+					Les informations quotidiennes <br /> <b>en un clin d'oeil</b>
+				</h1>
+				<img id="img_epaper" src="./assets/img/doigts.png">
+				</center> 
+			</div>
---
+++ includes/village.php
@@ -0,0 +1,8 @@
+			<div class="layer">
+
+					<h1 id="texte_village">
+					Les informations quotidiennes <br /> <b>en un clin d'oeil</b>
+				</h1>
+				<img id="img_village" src="./assets/img/doigts.png">
+
+			</div>
