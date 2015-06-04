fbe489ef5e84532984f780fec54fe9f1fe9b2459
comatik <loic.mancino.private@gmail.com>
2015-06-04 12:33:51 +0200

    Panel concours terminé

--- includes/concours.php
+++ includes/concours.php
@@ -1,4 +1,4 @@
-Envie d'un peu de challenge ? <br />  
+<h1>Envie d'un peu de challenge ? <br />  
 Gagner de super prix ? <br />
-Allez ! tente ta chance ! <br />
-<img src="./assets/img/games.png">
+Allez ! tente ta chance ! <br /></h1>
+<img id="img_concours" src="./assets/img/games.png">
