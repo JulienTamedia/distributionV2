6ba797d23221a4391f9f480decb6fa6cf5cc3df5
comatik <loic.mancino.private@gmail.com>
2015-06-07 20:46:19 +0200

    Modification du panel météo

--- includes/meteo.php
+++ includes/meteo.php
@@ -5,50 +5,84 @@ $json = json_decode($json);
  ?>
 
  <?php 
+ echo '<div id="current_day">';
   echo $json->city_info->name;
   echo '/';
   echo $json->current_condition->tmp;
   echo '°';
   echo '<img src="'.$json->fcst_day_0->icon_big.'"/>';
-
-  echo $json->fcst_day_1->day_short;
+echo '</div>';
+echo '<div class="day_after"><p>';
+  echo trim(substr($json->fcst_day_1->day_short, 0, -2));
   echo '/';
   echo $json->fcst_day_1->tmax;
-  echo '<img src="'.$json->fcst_day_1->icon.'"/>';
-
-  echo $json->fcst_day_2->day_short;
+  echo '</p><img src="'.$json->fcst_day_1->icon.'"/>';
+echo '</div>';
+echo '<div class="day_after"><p>';
+  echo trim(substr($json->fcst_day_2->day_short, 0, -2));
   echo '/';
   echo $json->fcst_day_2->tmax;
-  echo '<img src="'.$json->fcst_day_2->icon.'"/>';
-
-  echo $json->fcst_day_3->day_short;
+  echo '</p><img src="'.$json->fcst_day_2->icon.'"/>';
+echo '</div>';
+echo '<div class="day_after"><p>';
+  echo trim(substr($json->fcst_day_3->day_short, 0, -2));
   echo '/';
   echo $json->fcst_day_3->tmax;
-  echo '<img src="'.$json->fcst_day_3->icon.'"/>';
+  echo '</p><img src="'.$json->fcst_day_3->icon.'"/>';
+echo '</div>';
 
-  echo $json->fcst_day_4->day_short;
-  echo '/';
-  echo $json->fcst_day_4->tmax;
-  echo '<img src="'.$json->fcst_day_4->icon.'"/>';
   ?>
 
 
 
 <style>
 @import url(http://fonts.googleapis.com/css?family=Quicksand);
+
 *{
 	margin: 0;
 	padding:0;
-	font-family:'Quicksand', sans-serif;
+	font-family:'Quicksand';
     font-style: normal;
     font-weight: normal;
     text-transform: uppercase;
+    color: white;
+
 }
 
-				img{
-				height: 50%;
-				width: auto;
+.day_after{
+display: inline-block;
+  margin: 2px 30px 2px 30px;
+  text-align: center;
+
+}
+
+#current_day {
+  display: inline-block;
+}
+div{
+  
+}
+
+			#current_day	img{
+        height: 70px;
+          width: auto;
+        margin-right: 45px;
 			}
+
+      .day_after  img{
+        height: 50px;
+          width: auto;
+          filter: grayscale(1);
+        -webkit-filter: grayscale(1);
+        -moz-filter: grayscale(1);
+        -o-filter: grayscale(1);
+        -ms-filter: grayscale(1);
+      }
+
+
+      p{
+
+      }
 </style>
 
 
